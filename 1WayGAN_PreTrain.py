2dda8ce2fb585706e08773aa7508aad585e0c9f2
MrRobot2211 <felipebihaiek@gmail.com>
2020-04-22 15:42:43 -0300

    2waygan v2

--- 1WayGAN_PreTrain.py
+++ 1WayGAN_PreTrain.py
@@ -47,25 +47,23 @@ if __name__ == "__main__":
             # Print statistics
             running_loss += loss.item()
             running_losslist.append(loss.item())
-            f = open("./models/log_PreTraining.txt", "a+")
-            f.write("[Epoch %d/%d] [Batch %d/%d] [G loss: %f]\n" % (
-                epoch + 1, NUM_EPOCHS_PRETRAIN + 1, i + 1, len(trainLoader1), loss.item()))
-            f.close()
-            # if i % 200 == 200:    # print every 200 mini-batches
-            if i % 1 == 0:
-                print('[%d, %5d] loss: %.5f' % (epoch + 1, i + 1, running_loss / 5))
-                running_loss = 0.0
+            
+            if epoch >= (NUM_EPOCHS_PRETRAIN -1):    # print every 200 mini-batches
+
+                
+           
                 save_image(generated_enhanced_image.data,
-                           "./models/pretrain_images/gan1_pretrain_%d_%d.png" % (epoch + 1, i + 1),
-                           nrow=8,
-                           normalize=True)
+                        "./models/pretrain_images/gan1_pretrain_%d_%d.png" % (epoch + 1, i + 1),
+                        nrow=8,
+                        normalize=True)
                 torch.save(generator.state_dict(),
-                           './models/pretrain_checkpoint/gan1_pretrain_' + str(epoch + 1) + '_' + str(i + 1) + '.pth')
+                        './models/pretrain_checkpoint/gan1_pretrain_' + str(epoch + 1) + '_' + str(i + 1) + '.pth')
 
-    end_time = datetime.now()
-    print(end_time-start_time)
-
-    f = open("./models/log_PreTraining_LossList.txt", "a+")
-    for item in running_losslist:
-        f.write('%f\n' % item)
-    f.close()
+                end_time = datetime.now()
+                print(end_time-start_time)
+            print('[%d, %5d] loss: %.5f' % (epoch + 1, i + 1, running_loss / 5))
+            f = open("./models/log_PreTraining_LossList.txt", "a+")
+            for item in running_losslist:
+                f.write('%f\n' % item)
+            f.close()
+            running_loss = 0.0
